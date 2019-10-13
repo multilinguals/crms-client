3618170ba93dc2c246c2d15a8c0f9fd226074c75
sidfate <15757176627@163.com>
2019-10-13 15:24:27 +0800

    :sparkles: home pages.

---
+++ src/views/home/Index.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-container fluid>
+    <v-row>
+      <v-col>
+        <v-card
+                color="#385F73"
+                dark
+        >
+          <v-card-text class="white--text">
+            <div class="headline mb-2">{{ group.name }}</div>
+            {{ group.description }}
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn text @click="onOpenDetail(group.groupId)">查看详情</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  import { getGroup } from "@/apis/group"
+
+  export default {
+    name: "Home",
+    data: () => {
+      return {
+        group: {
+          name: '',
+          description: ''
+        }
+      }
+    },
+    created() {
+      getGroup().then(res => {
+        this.group = Object.assign({}, res)
+      })
+    },
+    methods: {
+      onOpenDetail(id) {
+        console.log('groupId:'+id)
+        this.$router.push({
+          name: 'group_detail',
+          params: {id}
+        })
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/home/My.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-container fluid>
+    <v-row>
+      <v-col>
+        <v-card
+                color="#385F73"
+                dark
+        >
+          <v-card-text class="white--text">
+            <div class="headline mb-2">xxxx餐厅</div>
+            My
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn text>2019年09月15日</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  import { getGroup } from "@/apis/group"
+
+  export default {
+    name: "My",
+    created() {
+      getGroup().then(res => {
+        console.log(res)
+      })
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
